a37a9d096c0fe6408fd93b1e88436fc02ed7315a
zfy <1690445989@qq.com>
2019-03-06 12:14:58 +0800

    提交

---
+++ com.itzfy/src/com/itzfy/hello/Helllo.java
@@ -0,0 +1,7 @@
+package com.itzfy.hello;
+
+public class Helllo {
+public static void main(String[] args){
+	System.out.println("hello world");
+}
+}
---
+++ com.itzfy/src/com/itzfy/hello/MethodDemo.java
@@ -0,0 +1,11 @@
+package com.itzfy.hello;
+
+public class MethodDemo {
+	public static void main(String[] args) {
+		int sum=getSum(10,20);
+		System.out.println(sum);
+	}
+	public static int getSum(int a,int b){
+		return a+b;
+	}
+}
